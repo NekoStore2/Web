d87eb75123a49acf101333ad14be17c69ee6f2c0
NekoStore2 <164801435+NekoStore2@users.noreply.github.com>
2025-03-29 05:16:59 +0700

    Update Gemini.js

--- Api/Gemini/Api/Gemini.js
+++ Api/Gemini/Api/Gemini.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     }
 
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${process.env.AIzaSyCulirvs9ecKy0z023LXZZY-C-QpAajnjQ}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(req.body),
